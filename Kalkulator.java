8c472529d584350deda14de8ca0ac5780967a80e
username <email>
2024-02-12 09:48:11 +0700

    memancing perkoro

--- Kalkulator.java
+++ Kalkulator.java
@@ -13,6 +13,7 @@ public class Kalkulator {
         System.out.println("2. pengurangan");
         System.out.println("3. perkalian");
         System.out.println("4. pembagian");
+        System.out.println("5. Hasil bagi");
 
         System.out.println("masukan pilihan (1/2/3/4) : ");
         String pilihan = scanner.nextLine();
