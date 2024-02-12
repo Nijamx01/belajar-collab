a81e15c04b4118055d70f818633290b6ea199aa9
username <email>
2024-02-12 10:54:59 +0700

    5 > 6

--- Kalkulator.java
+++ Kalkulator.java
@@ -16,7 +16,7 @@ public class Kalkulator {
         System.out.println("5. Hasil bagi");
        
 
-        System.out.println("masukan pilihan (1/2/3/4/6) : ");
+        System.out.println("masukan pilihan (1/2/3/4/5) : ");
         String pilihan = scanner.nextLine();
 
         if (pilihan.equals("1") || pilihan.equals("2") || pilihan.equals("3") || pilihan.equals("4")) {
